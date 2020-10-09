13320b4e72dd5e19934591e98e3145e5a1f16f80
linjie11 <Lilinjie123>
2020-10-09 21:20:13 +0800

    狂神Java完整版SSM

---
+++ ssmbuild/src/main/java/com/linjie/service/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package com.linjie.service;
+
+import com.linjie.dao.BookMapper;
+import com.linjie.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+
+    public Books queryBookByName(String bookName) {
+        return bookMapper.queryBookByName(bookName);
+    }
+}
